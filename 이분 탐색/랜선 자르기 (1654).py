281e13107db5e37c8d06060b10c9d33d54ac34b7
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-09-22 18:40:42 +0900

    Update 랜선 자르기 (1654).py

--- 이분 탐색/랜선 자르기 (1654).py
+++ 이분 탐색/랜선 자르기 (1654).py
@@ -6,7 +6,7 @@ K, N = map(int,input().rstrip().split())
 line = [int(input()) for i in range(K)]
 start, end = 1, max(line)
 
-while start < end:
+while start <= end:
     mid = (start+end)//2
     lines = 0
     for i in line:
