0014ecf6dff49184186e4f0b9501fa588f8e640a
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-09-25 15:44:48 +0900

    가장 긴 증가하는 부분 수열2

---
+++ 이분 탐색/가장 긴 증가하는 부분 수열2 (12015)/가장 긴 증가하는 부분 수열2 (12015).py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+list = list(map(int, input().rstrip().split()))
+stack = [0]
+
+def binary_search(target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if stack[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+
+for i in list:
+    if stack[-1] < i:
+        stack.append(i)
+    else:
+        stack[binary_search(i, 0, len(stack) - 1)] = i
+
+print(len(stack) - 1)
