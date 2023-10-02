0f771c0b2177cb8794d4c9ef1c6a37e378fc9276
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-02 17:32:57 +0900

    S1 카드 구매하기 2

---
+++ 동적 계획법/카드 구매하기2 (16194)/카드 구매하기2 (16194).py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+cost = [0] + list(map(int, input().rstrip().split()))
+dp = [0 for i in range(N+1)]
+
+dp[1] = cost[1]
+
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        if dp[i]:
+            dp[i] = min(dp[i], cost[j]+dp[i-j])
+        else:
+            dp[i] = dp[i-j]+cost[j]
+print(dp)
