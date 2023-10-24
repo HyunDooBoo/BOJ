91f9b10fa4b9a2f08b0178b19991dbc6ac2f83b5
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-24 23:47:51 +0900

    타일 채우기 (2133)

---
+++ 동적 계획법/타일 채우기 (2133)/타일 채우기 (2133).py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0 for i in range(N+1)]
+dp[0] = 1
+for i in range(2, N+1, 2):
+    dp[i] += dp[i-2] * 3
+    for j in range(0,i-2,2):
+        dp[i] += dp[j]*2
+
+print(dp[N])
