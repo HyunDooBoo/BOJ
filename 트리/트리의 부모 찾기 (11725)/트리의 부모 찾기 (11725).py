7d592cb64a5da3e6b1820732860b0de0e983e560
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-02-10 23:54:26 +0900

    S2 트리의 부모 찾기 (11725)

---
+++ 트리/트리의 부모 찾기 (11725)/트리의 부모 찾기 (11725).py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+input = sys.stdin.readline
+
+def dfs(start):
+    for i in graph[start]:
+        if visited[i] == False:
+            visited[i] = True
+            answer[i] = start
+            dfs(i)
+
+N = int(input().rstrip())
+graph = [[] for i in range(N+1)]
+
+for i in range(N-1):
+    a, b = map(int, input().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (N+1)
+answer = [0] * (N+1)
+dfs(1)
+
+for i in range(2, N+1):
+    print(answer[i])
