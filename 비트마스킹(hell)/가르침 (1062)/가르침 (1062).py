15492060f3716e3cc94ba08c8ad2292c111444c5
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-04-23 17:32:11 +0900

    G4 가르침 (1062)
    기존 dfs로 풀었던 걸 비트마스킹으로 재풀이

---
+++ 비트마스킹(hell)/가르침 (1062)/가르침 (1062).py
@@ -0,0 +1,26 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+def word2bit(word):
+    bit = 0
+    for char in word:
+        bit = bit | (1 << ord(char) - ord('a'))
+    return bit
+
+N, K = map(int, input().rstrip().split())
+words = [input().rstrip() for _ in range(N)]
+bits = list(map(word2bit, words))
+base_bit = word2bit('antic')
+
+alphabet = [1 << i for i in range(26) if not (base_bit & 1 << i)]
+answer = 0
+for combination in combinations(alphabet, K-5):
+    know_bit = sum(combination) | base_bit
+    count = 0
+    for bit in bits:
+        if bit & know_bit == bit:
+            count += 1
+    answer = max(answer, count)
+print(answer)
