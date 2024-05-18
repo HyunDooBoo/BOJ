4f92cfc0771ae8041f6bbe93253e01c6f18ce964
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-05-19 00:27:19 +0900

    Update 극장 좌석 (2302).py

--- 동적 계획법/극장 좌석 (2302)/극장 좌석 (2302).py
+++ 동적 계획법/극장 좌석 (2302)/극장 좌석 (2302).py
@@ -5,12 +5,12 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 M = int(input().rstrip())
 vips = list(int(input().rstrip()) for i in range(M))
-dp = [0 for i in range(N+1)]
+dp = [0 for i in range(41)]
 dp[0] = 1
 dp[1] = 1
 dp[2] = 2
 
-for i in range(3, N+1):
+for i in range(3, 41):
     dp[i] = dp[i-1] + dp[i-2]
 
 answer = 1
@@ -18,9 +18,11 @@ answer = 1
 if M > 0:
     tmp = 0
     for i in vips:
-        answer *= dp[i - tmp - 1]
+        if i - tmp - 1 >= 0:
+            answer *= dp[i - tmp - 1]
         tmp = i
-    answer *= dp[N-tmp]
+    if N-tmp >= 0:
+        answer *= dp[N-tmp]
 else:
     answer = dp[N]
 print(answer)
